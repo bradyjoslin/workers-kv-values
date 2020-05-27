1a21a227382cc5ab8547ac54180943a4ceef77bc
Brady Joslin <brady@bradyjoslin.com>
2020-05-27 17:14:18 -0500

    Update get_kvs.sh

--- get_kvs.sh
+++ get_kvs.sh
@@ -13,21 +13,22 @@
 #               jq            https://stedolan.github.io/jq/
 
 USAGE=$(cat <<-END
-Creates a JSON array of all key values stored in a
-Workers KV namespace, where the values are strings.\n\n
-FLAGS:\n
-              \t-h\tPrints help information\n\n
-ARGS:\n
-              \t<namespace> [Required] \tWorkers kv namespace\n
-              \t<environment>\t\tDefaults to none (dev environment)\n\n
-Dependencies:\n
-              \twrangler - https://github.com/cloudflare/wrangler\n
-              \tjq - https://stedolan.github.io/jq/\n
+Creates a JSON array of all key values stored in a Workers KV namespace, 
+where the values are strings.
+
+FLAGS:
+              -h    Prints help information
+ARGS:
+              <namespace>    [Required]    Workers kv namespace
+              <environment>                Defaults to none (dev environment)
+Dependencies:
+              wrangler - https://github.com/cloudflare/wrangler
+              jq - https://stedolan.github.io/jq/
 END
 )
 
 if [ "${1}" == "-h" ]; then
-  echo -e ${USAGE}
+  echo "${USAGE}"
   exit 0
 fi
 
@@ -37,7 +38,7 @@ kv_environment_in=${2};
 if [ -z "${kv_namespace_in}" ]
 then
     echo -e "Error! namespace argument required\n"
-    echo -e ${USAGE}
+    echo "${USAGE}"
     exit 1
 fi
 
